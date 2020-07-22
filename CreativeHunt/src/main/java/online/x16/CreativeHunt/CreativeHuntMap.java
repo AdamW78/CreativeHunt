bb871e39e9816bc02d860870b9c2b0e42796b388
Adam W <Code.AdamW78@gmail.com>
2020-07-21 21:18:07 -0400

    Added put, remove and toggle methods for CreativeHuntMap

--- CreativeHunt/src/main/java/online/x16/CreativeHunt/CreativeHuntMap.java
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/CreativeHuntMap.java
@@ -19,12 +19,44 @@ public class CreativeHuntMap {
 	/**
 	 * Puts a Player p into a CreativeHuntMap
 	 * @param p Player to be put into CreativeHuntMap
+	 * @return boolean Whether or not Player was successfully added
 	 */
-	public void put(Player p) {
-		if (!contains(p)) {
-			
+	public boolean put(Player p) {
+		if (contains(p)) {
+			return false;
+		}
+		else {
+			map.put(p, new ScheduledThreadPoolExecutor(1));
+			return true;
 		}
 	}
+	/**
+	 * Removes a Player p from a CreativeHuntMap
+	 * @param p Player to remove from the CreativeHuntMap
+	 * @return boolean Whether or not Player was successfully removed
+	 */
+	public boolean remove(Player p) {
+		if (map.remove(p) != null) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * Toggles whether or not Player p is in the CreativeHuntMap
+	 * @param p Player to toggle in the CreativeHuntMap
+	 * @return boolean Whether or not the player is in CreativeHuntMap after the method call
+	 */
+	public boolean toggle (Player p) {
+		if (contains(p)) {
+			return remove(p);
+		}
+		return put(p);
+	}
+	/**
+	 * Checks whether a player is in a CreativeHuntMap
+	 * @param p Player to check in the CreativeHuntMap
+	 * @return boolean Whether or not the player was in the CreativeHuntMap
+	 */
 	public boolean contains(Player p) {
 		return map.containsKey(p);
 	}
