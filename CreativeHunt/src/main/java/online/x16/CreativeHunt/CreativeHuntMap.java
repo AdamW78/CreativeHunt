f80b250d4c5447da7667861f49874691da1a76c4
Adam W <Code.AdamW78@gmail.com>
2020-07-21 23:35:34 -0400

    Added target player to CreativeHuntMap

--- CreativeHunt/src/main/java/online/x16/CreativeHunt/CreativeHuntMap.java
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/CreativeHuntMap.java
@@ -1,5 +1,6 @@
 package online.x16.CreativeHunt;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -11,7 +12,7 @@ import online.x16.CreativeHunt.tools.MessageBuilder;
 public class CreativeHuntMap {
 
 	private CreativeHunt plugin;
-	private HashMap<Player, ScheduledThreadPoolExecutor> map;
+	private HashMap<Player, ArrayList<Object>> map;
 	private MessageBuilder messageBuilder;
 	/**
 	 * Constructor for CreativeHuntMap so that config values can be passed in through a CreativeHunt instance
@@ -26,12 +27,15 @@ public class CreativeHuntMap {
 	 * @param p Player to be put into CreativeHuntMap
 	 * @return boolean Whether or not Player was successfully added
 	 */
-	public boolean put(Player p) {
+	public boolean put(Player p, Player target) {
 		if (contains(p)) {
 			return false;
 		}
 		else {
-			map.put(p, new ScheduledThreadPoolExecutor(1));
+			ArrayList<Object> timerTargetList = new ArrayList<Object>();
+			timerTargetList.add(new ScheduledThreadPoolExecutor(1));
+			timerTargetList.add(target);
+			map.put(p, new ArrayList<Object>());
 			startSurvivalTimer(p);
 			return true;
 		}
@@ -52,11 +56,11 @@ public class CreativeHuntMap {
 	 * @param p Player to toggle in the CreativeHuntMap
 	 * @return boolean Whether or not the player is in CreativeHuntMap after the method call
 	 */
-	public boolean toggle (Player p) {
+	public boolean toggle (Player p, Player target) {
 		if (contains(p)) {
 			return remove(p);
 		}
-		return put(p);
+		return put(p, target);
 	}
 	/**
 	 * Checks whether a player is in a CreativeHuntMap
@@ -71,7 +75,7 @@ public class CreativeHuntMap {
 	 * @param p Player to put into survival
 	 */
 	public void cancelSurvivalTimer(Player p) {
-		map.get(p).shutdown();
+		((ScheduledThreadPoolExecutor) map.get(p).get(0)).shutdown();
 	}
 	
 	/**
@@ -79,7 +83,7 @@ public class CreativeHuntMap {
 	 * @param p PLayer to set a timer for
 	 */
 	public void startSurvivalTimer(Player p) {
-		map.get(p).schedule(new GamemodeCallable(p), plugin.getConfig().getInt("creative-seconds"), TimeUnit.SECONDS);
+		((ScheduledThreadPoolExecutor) map.get(p).get(0)).schedule(new GamemodeCallable(p), plugin.getConfig().getInt("creative-seconds"), TimeUnit.SECONDS);
 		p.spigot().sendMessage(messageBuilder.build("&7You have &7"+plugin.getConfig().getInt("creative-seconds")+"&7 seconds in creative mode."));
 		
 	}
---
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/PlayerInteractListener.java
@@ -0,0 +1,30 @@
+package online.x16.CreativeHunt;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class PlayerInteractListener implements Listener {
+
+	private CreativeHunt plugin;
+	
+	public PlayerInteractListener(CreativeHunt instance) {
+		plugin = instance;
+	}
+	
+	public void onPlayerInteract(PlayerInteractEvent e) {
+		//Double check that the Player who interacted is a Player
+		if(e.getPlayer() instanceof Player) {
+			//Store Player
+			Player p = e.getPlayer();
+			//Check if the Player p is in CreativeHunt mode - if not, stop everything here
+			if (!plugin.getMap().contains(p)) return;
+			//Check if the item the player just interacted with (in their main hand) was a compass
+			if (p.getInventory().getItemInMainHand().getType().equals(Material.COMPASS)) {
+				p.setCompassTarget(loc);
+			}
+		}
+	}
+	
+}
