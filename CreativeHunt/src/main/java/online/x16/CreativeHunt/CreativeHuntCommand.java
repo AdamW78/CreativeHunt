d10f89d6311702ec648f1fe0be88658cc5083855
Adam W <Code.AdamW78@gmail.com>
2020-07-21 20:27:19 -0400

    Added Command class and Map class

---
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/CreativeHuntCommand.java
@@ -0,0 +1,15 @@
+package online.x16.CreativeHunt;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CreativeHuntCommand implements CommandExecutor {
+	
+	@Override   
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		   
+		   return false;
+	}
+	
+}
---
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/CreativeModeMap.java
@@ -0,0 +1,15 @@
+package online.x16.CreativeHunt;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
+public class CreativeModeMap {
+
+	private HashMap<Player, ArrayList<Object>> map;
+	
+	public CreativeModeMap() {
+		
+	}
+}
