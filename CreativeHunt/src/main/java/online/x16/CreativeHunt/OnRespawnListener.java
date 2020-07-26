c2528daae451690f7a67c3b896da432cb86feceb
Adam W <Code.AdamW78@gmail.com>
2020-07-26 17:31:26 -0400

    Added creative mode on respawn

--- CreativeHunt/src/main/java/online/x16/CreativeHunt/OnRespawnListener.java
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/OnRespawnListener.java
@@ -1,5 +1,6 @@
 package online.x16.CreativeHunt;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -14,6 +15,7 @@ public class OnRespawnListener implements Listener {
 	
 	public void onPlayerRespawn(PlayerRespawnEvent e) {
 		if(plugin.getMap().contains(e.getPlayer())) {
+			e.getPlayer().setGameMode(GameMode.CREATIVE);
 			plugin.getMap().startSurvivalTimer(e.getPlayer());
 			tracker = new TrackerCompass(plugin);
 			tracker.giveTrackerCompass(e.getPlayer());
