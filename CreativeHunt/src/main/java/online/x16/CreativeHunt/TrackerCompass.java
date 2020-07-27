913105d97123f1956f18343b9040047dd968ec9d
Adam W <Code.AdamW78@gmail.com>
2020-07-27 13:44:13 -0400

    Removed unused imports

--- CreativeHunt/src/main/java/online/x16/CreativeHunt/TrackerCompass.java
+++ CreativeHunt/src/main/java/online/x16/CreativeHunt/TrackerCompass.java
@@ -1,22 +1,11 @@
 package online.x16.CreativeHunt;
 
 import java.util.*;
-
-import com.google.common.collect.Multimap;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.attribute.AttributeModifier;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
-import org.bukkit.inventory.meta.ItemMeta;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
-import org.bukkit.persistence.PersistentDataContainer;
 
 public class TrackerCompass {
 	
@@ -37,7 +26,7 @@ public class TrackerCompass {
 		if (trackerCompass.getItemMeta() instanceof CompassMeta) {
 			CompassMeta trackerMeta = (CompassMeta) trackerCompass.getItemMeta();
 			trackerMeta.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Tracker Compass");
-			List<String> trackerLore = new ArrayList<String>();
+			ArrayList<String> trackerLore = new ArrayList<String>();
 			trackerLore.add(ChatColor.GRAY + "Hunt down your enemies!");
 			trackerLore.add(ChatColor.GRAY + "Right-click to update your target");
 			trackerMeta.setLore(trackerLore);
